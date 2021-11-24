a8eea9312b378f77b3d9fe9c932e2f856147ca32
Mikko Kasanen <kasanen_mikko@hotmail.com>
2021-11-24 09:12:54 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPAddress broadcast;
+
+        if (args[2] == "ip") {
+            broadcast = IPAddress.Parse(args[0]);
+        }
+        else {
+            broadcast = Dns.GetHostEntry(args[0]).AddressList[0];
+        }
+        
+        
+        Console.WriteLine(broadcast);
+
+        byte[] sendbuf = Encoding.ASCII.GetBytes(args[1]);
+        
+        IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+        s.SendTo(sendbuf, ep);
+
+        Console.WriteLine("Message sent to the broadcast address");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+### Building client
+
+dotnet build
+
+### Running Client with DNS address
+
+dotnet run google.fi hello dns
+
+### Running Client with IP address
+
+dotnet run 127.0.0.1 hello ip
